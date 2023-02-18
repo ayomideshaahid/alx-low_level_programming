ff2b2d72b638120f9ffe40641b81920234885a41
ayomideshaahid <ayomideopeyemi4210@gmail.com>
2023-02-17 17:40:40 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-include<stdio.h>
+#include<stdio.h>
 /**
- * main - entry
- * * Return: 1
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
 
