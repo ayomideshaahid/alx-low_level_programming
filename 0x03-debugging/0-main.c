a847942976e0eb334e266efe14085769f0f77aaf
ayomideshaahid <ayomideopeyemi4210@gmail.com>
2023-02-22 08:12:08 -0800

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "MAIN_H"
+
+/**
+ * main - test function that prints if intiger is positive or nrgative
+ */
+
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative i;
+
+	return (0);
+}
