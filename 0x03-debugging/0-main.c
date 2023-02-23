95be61f4f6648fb75cdbe2566a6c049ed90483c2
ayomideshaahid <ayomideopeyemi4210@gmail.com>
2023-02-23 02:38:37 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,18 @@
 #include "MAIN_H"
 
 /**
- * main - test function that prints if intiger is positive or nrgative
+ * main - test funtion that prints if intiger is positive or negative
+ * Return: 0
  */
+
 
 
 int main(void)
 {
-
+	
 	int i;
-
 	i = 0;
-	positive_or_negative i;
+	positive_or_negative(i);
 
-	return (0);
+	return: 0;
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-this is my project on debugging
+0x03-debugging
