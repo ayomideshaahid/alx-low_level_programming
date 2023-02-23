21f00dfbe7aacfa189603ffc9b93dfbe1307bf28
ayomideshaahid <ayomideopeyemi4210@gmail.com>
2023-02-23 15:53:52 -0800

    task0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * file - main.h
+ * Authors: Genius Village
+ * Desc: Header contains protypes for all functions
+ 	used in 0x03-more_function_loops_directory
+*/
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+#endif
